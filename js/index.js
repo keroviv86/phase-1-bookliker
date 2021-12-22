f6d14995283269317e3cd6327b9df153c271ecb8
Vivian Yim <keroviv86@gmail.com>
2021-12-21 22:47:13 -0600

    commit tree 8c0981ff31041c38dee212d4e4ab3588596efd3f

--- js/index.js
+++ js/index.js
@@ -1,1 +1,75 @@
-document.addEventListener("DOMContentLoaded", function() {});
+init();
+
+function init(){
+    getBooks()
+}
+
+//fetch
+function getBooks(){
+    fetch('http://localhost:3000/books')
+    .then(res => res.json())
+    .then(bookdata=> bookdata.forEach(book => showTitle(book)))
+}
+
+function showTitle(books){
+    const list = document.getElementById('list')
+    let bookTitle = document.createElement('li')
+    bookTitle.textContent = books.title 
+
+    list.appendChild(bookTitle)
+
+    bookTitle.addEventListener('click', ()=> showBookDetails(books))
+}
+
+
+function showBookDetails(books){
+    const showBook = document.getElementById('show-panel')
+
+    let bookImg = document.createElement('img')
+    bookImg.src = books.img_url
+
+    let bookTitle = document.createElement('h1')
+    bookTitle.textContent= books.title
+
+    let bookSubtitle = document.createElement('h2')
+    bookSubtitle.textContent = books.subtitle
+
+    let bookAuthor = document.createElement('h2')
+    bookAuthor.textContent= books.author
+
+    let bookDescription = document.createElement('h3')
+    bookDescription.textContent= books.description
+
+    const ul= document.createElement('ul')
+
+    for(const user of books.users) {
+        const li = document.createElement('li')
+        li.textContent = user.username
+        ul.append(li)
+    }
+
+    //or use forEach
+    // books.users.forEach(user=> {
+    //     const li = document.createElement('li')
+    //     li.textContent = user.username
+    //     ul.append(li)
+    // })
+
+    let likeButton = document.createElement('button')
+    likeButton.textContent= 'LIKE'
+    //want to click on button 
+    likeButton.addEventListener('click', (e)=> clickButton(e))
+
+
+    showBook.innerText = ''
+    showBook.append(bookImg, bookTitle, bookSubtitle, bookAuthor, bookDescription,ul, likeButton)
+
+}
+
+function clickButton(e){
+    if(e.target.innerText=== "LIKE"){
+        e.target.innerText= "UNLIKE"
+    }else{
+        e.target.innerText="LIKE"
+    }
+}
